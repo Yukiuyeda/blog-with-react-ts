b44afdb69069c83db01dd77ecce8cf97e62d891a
yukiuyeda <yueda940408@gmail.com>
2024-06-09 16:29:48 +0900

    chapter7課題

--- src/components/DetailPost/DetailPost.tsx
+++ src/components/DetailPost/DetailPost.tsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 // import { posts } from '../../data/posts';
 import styles from "./DetailPost.module.css";
-import type { Post, PostType } from "../../data/post";
+import type { Post, PostType, PostsType } from "../../data/post";
 
 const DetailPost = () => {
   const [post, setPost] = useState<Post>();
@@ -24,6 +24,9 @@ const DetailPost = () => {
     fetcher();
   }, []);
 
+
+  console.log(post);
+
   //ローディング中の表示
   if (isLoading) {
     return <div>読み込み中…</div>;
@@ -36,26 +39,26 @@ const DetailPost = () => {
 
   if (post !== undefined) {
     return (
-      <div className={styles.container}>
-        <img src={post.thumbnailUrl} />
-        <div className={styles.article}>
-          <div className={styles.dateAndCategories}>
-            <p className={styles.date}>
-              {new Date(post.createdAt).toLocaleDateString()}
-            </p>
-            <ul className={styles.categories}>
-              {post.categories.map((category) => {
-                return <li key={category}>{category}</li>;
-              })}
-            </ul>
+        <div className={styles.container}>
+          <img src={post.thumbnailUrl} />
+          <div className={styles.article}>
+            <div className={styles.dateAndCategories}>
+              <p className={styles.date}>
+                {new Date(post.createdAt).toLocaleDateString()}
+              </p>
+              <ul className={styles.categories}>
+                {post.categories.map((category) => {
+                  return <li key={category}>{category}</li>;
+                })}
+              </ul>
+            </div>
+            <h3 className={styles.title}>{`APIで取得した${post.title}`}</h3>
+            <div
+              className="detailContent"
+              dangerouslySetInnerHTML={{ __html: post.content }}
+            ></div>
           </div>
-          <h3 className={styles.title}>{`APIで取得した${post.title}`}</h3>
-          <div
-            className="detailContent"
-            dangerouslySetInnerHTML={{ __html: post.content }}
-          ></div>
         </div>
-      </div>
     );
   }
 };
